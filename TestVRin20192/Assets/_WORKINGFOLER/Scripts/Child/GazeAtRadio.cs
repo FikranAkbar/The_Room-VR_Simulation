7957481e91f64ba229fb00017228f0433a5601c4
FikranAkbar <44489539+FikranAkbar@users.noreply.github.com>
2021-01-14 08:47:51 +0700

    Adding pos processing, sound

---
+++ TestVRin20192/Assets/_WORKINGFOLER/Scripts/Child/GazeAtRadio.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GazeAtRadio : MonoBehaviour
+{
+    // images for icon
+    private Image whiteIcon;
+    private Image redIcon;
+
+    // coroutine for triggering sound
+    private Coroutine lastGazingCoroutine;
+
+    // times property for gazing
+    private float gazeTime;
+    [SerializeField]
+    private float clickDuration;
+
+    // sounds
+    public List<GameObject> studioEventsEmitter;
+    private int randomNum;
+    private GameObject eventPlaying;
+
+    // transforms
+    public Transform playerVR;
+    public float invisibleAfter;
+
+    // booleans property for gazing
+    private bool isGazing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        whiteIcon = transform.GetChild(0).GetComponent<Image>();
+        redIcon = transform.GetChild(1).GetComponent<Image>();
+        eventPlaying = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGazing)
+        {
+            gazeTime += Time.deltaTime;
+            redIcon.fillAmount = gazeTime / clickDuration;
+        }
+        else
+        {
+            if (lastGazingCoroutine != null)
+            {
+                StopCoroutine(lastGazingCoroutine);
+                lastGazingCoroutine = null;
+            }
+            gazeTime = 0f;
+            redIcon.fillAmount = gazeTime;
+        }
+
+        if (redIcon.fillAmount >= 1f)
+        {
+            whiteIcon.enabled = false;
+            redIcon.enabled = false;
+        }
+        else
+        {
+            whiteIcon.enabled = true;
+            redIcon.enabled = true;
+        }
+    }
+
+    public void StartGazing()
+    {
+        if (Vector3.Distance(playerVR.position, transform.position) <= invisibleAfter)
+        {
+            isGazing = true;
+            if (lastGazingCoroutine == null)
+            {
+                lastGazingCoroutine = StartCoroutine(WaitGazingTillFinish());
+            }
+        }
+    }
+
+    public void StopGazing()
+    {
+        isGazing = false;
+    }
+
+    private IEnumerator WaitGazingTillFinish()
+    {
+        Debug.Log("Start Coroutine");
+        yield return new WaitUntil(() => redIcon.fillAmount >= 1f);
+        if (eventPlaying == null)
+        {
+            randomNum = Random.Range(0, studioEventsEmitter.Count);
+            eventPlaying = studioEventsEmitter[randomNum];
+            eventPlaying.SetActive(true);
+        }
+        else if (eventPlaying != null)
+        {
+            eventPlaying.SetActive(false);
+            eventPlaying = null;
+        }
+    }
+}
---
+++ TestVRin20192/Assets/_WORKINGFOLER/Scripts/MoveTo.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveTo : MonoBehaviour
+{
+    // images for icon
+    private Image whiteIcon;
+    private Image redIcon;
+
+    // coroutine for triggering anim
+    private Coroutine lastGazingCoroutine;
+
+    // times property for gazing
+    private float gazeTime;
+    [SerializeField]
+    private float clickDuration;
+
+    // transforms
+    public Transform playerVR;
+    public Transform defaultTransform;
+    public Transform targetTransform;
+    private Vector3 initialPos;
+    private Vector3 bedPos;
+
+    // booleans property for gazing
+    private bool isGazing = false;
+
+    // game object to activate
+    public List<GameObject> gameObjectsToDeactivate;
+    public List<GameObject> gameObjectsToActivate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        whiteIcon = transform.GetChild(0).GetComponent<Image>();
+        redIcon = transform.GetChild(1).GetComponent<Image>();
+
+        initialPos = defaultTransform.transform.position;
+        bedPos = targetTransform.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGazing)
+        {
+            gazeTime += Time.deltaTime;
+            redIcon.fillAmount = gazeTime / clickDuration;
+        }
+        else
+        {
+            if (lastGazingCoroutine != null)
+            {
+                StopCoroutine(lastGazingCoroutine);
+                lastGazingCoroutine = null;
+            }
+            gazeTime = 0f;
+            redIcon.fillAmount = gazeTime;
+        }
+
+        //if (playerVR.position == targetTransform.position)
+        //{
+        //    whiteIcon.enabled = false;
+        //    redIcon.enabled = false;
+        //}
+        //else if (playerVR.position == defaultTransform.position)
+        //{
+        //    whiteIcon.enabled = true;
+        //    redIcon.enabled = true;
+        //}
+    }
+
+    public void StartGazing()
+    {
+        isGazing = true;
+        if (lastGazingCoroutine == null)
+        {
+            lastGazingCoroutine = StartCoroutine(WaitGazingTillFinish());
+        }
+    }
+
+    public void StopGazing()
+    {
+        isGazing = false;
+    }
+
+    private IEnumerator WaitGazingTillFinish()
+    {
+        Debug.Log("Start Coroutine");
+        yield return new WaitUntil(() => redIcon.fillAmount >= 1f);
+        playerVR.position = targetTransform.position;
+        
+        for (int i = 0; i<gameObjectsToActivate.Count; i++)
+        {
+            gameObjectsToActivate[i].SetActive(true);
+        }
+
+        for (int i = 0; i < gameObjectsToDeactivate.Count; i++)
+        {
+            gameObjectsToDeactivate[i].SetActive(false);
+        }
+
+        isGazing = false;
+        gazeTime = 0f;
+        redIcon.fillAmount = gazeTime;
+        gameObject.SetActive(false);
+    }
+}
